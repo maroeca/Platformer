312422a19278f4f57e8353e94df13a1ff6cf8d15
Maycow <maycow.carmo@gmail.com>
2024-11-04 19:10:21 -0300

    Adicionado calculo do score e rating
    - Mudado floating platform para fixedupdate

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -21,11 +21,88 @@ public class GameManager : MonoBehaviour
             // Caso já exista uma instância, destrói o novo objeto para garantir o Singleton
             Destroy(gameObject);
         }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
     #endregion
+    private float gameTime;
+    private bool isCounting;
 
     public int maxLives = 3;
     private int deaths = 0;
+
+    private int score;
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    private int startRating;
+
+    public int StarRating
+    {
+        get { return startRating; }
+        set { startRating = value; }
+    }
+
+    private int maxScore = 16000;
+
+    int[] starThresholds;
+
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Verifica se a cena carregada é a cena de Gameplay
+        if (scene.name == "Gameplay")
+        {
+            StartTimer();
+        }
+    }
+
+    private void Start()
+    {
+        starThresholds = new []{
+            (int)(0.2 * maxScore), // 1 estrela para 20% do score máximo
+            (int)(0.4 * maxScore), // 2 estrelas para 40% do score máximo
+            (int)(0.6 * maxScore), // 3 estrelas para 60% do score máximo
+            (int)(0.8 * maxScore), // 4 estrelas para 80% do score máximo
+            maxScore // 5 estrelas para 100% do score máximo
+        };
+    }
+
+    public void StartTimer()
+    {
+        gameTime = 0f;
+        isCounting = true;
+    }
+
+    public void StopTimer()
+    {
+        isCounting = false;
+        Debug.Log("Tempo final: " + gameTime + " segundos");
+    }
+
+    public float GetGameTime()
+    {
+        return gameTime;
+    }
+
+    private void Update()
+    {
+        if (isCounting)
+        {
+            gameTime += Time.deltaTime;
+            //Debug.Log(gameTime);
+        }
+    }
+
 
     public void RestartGame()
     {
@@ -49,15 +126,43 @@ public class GameManager : MonoBehaviour
         UIManager.Instance.RemoveLife();
     }
 
+    [ContextMenu("test")]
     public void FinishGame()
     {
         Debug.Log("Você terminou a fase!");
+        StopTimer();
+        CalculateScore();
         UIManager.Instance.ShowEndingPopup();
     }
 
+    
     public void CalculateScore()
     {
+        int scoreFactor = deaths > 0 ? deaths : 1;
+        int baseScore = 1000000;
+        Score = (int)(baseScore / gameTime) / scoreFactor;
 
+        CalculateStars(Score);
+        Debug.Log(Score);
+    }
+
+    public void CalculateStars(int score)
+    {
+        int stars = 0;
+
+        for (int i = 0; i < starThresholds.Length; i++)
+        {
+            if (score >= starThresholds[i])
+            {
+                stars = i + 1; // Incrementa uma estrela conforme o score alcança o próximo limite
+            }
+            else
+            {
+                break; // Para o loop se o score não alcançar o próximo limite
+            }
+        }
+
+        StarRating = stars;
     }
 
 }
--- Assets/Level/Scripts/PlatformController.cs
+++ Assets/Level/Scripts/PlatformController.cs
@@ -14,7 +14,7 @@ public class PlatformController : MonoBehaviour
    
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 target = CurrentMovementTarget();
 
